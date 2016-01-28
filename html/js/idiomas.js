5a78dd5c81182f9f82f52fde26561c8cc96498ba
Josu <pradosotx@gmail.com>
2016-01-28 10:25:21 +0100

    Este es el comienzo de todo

---
+++ html/js/idiomas.js
@@ -0,0 +1,7 @@
+function IdiomaEs(){
+    document.getElementById("link").href="css/style2.css";
+}
+
+function IdiomaEn(){
+    document.getElementById("link").href="css/style.css";
+}
---
+++ html/php/formulario.php
@@ -0,0 +1,21 @@
+﻿<?php
+
+$email = $_POST['email'];
+$asunto = $_POST['subject'];
+$mensaje = "Nombre: " . $_POST['name'] . "\nEmail: " . $_POST['email'] . "\nSkype: " . $_POST['skype'] . "\nMensaje: " . $_POST['message'];
+$headers = 'X-Mailer: PHP/'.phpversion()  . "From: " . $_POST['email'];
+
+
+if (mail("euweloboost@outlook.com", $asunto, $mensaje, $headers)) {
+
+	mail($email, "EUWELOBOOST.com: Consulta recibida", "Hola " . $_POST['name'] . ",\n\nHemos recibido su consulta, le responderemos en menos de 12h. \nPuede responder a este mensaje con cualquier duda, para más información y contacto directo puede agregarnos a Skype: euweloboost_1. \n\nUn saludo.", $headers);
+	?>
+	<script>
+		alert('\u00A1Consulta enviada! \nEn los pr\u00F3ximos minutos recibir\u00E1s un email de confirmaci\u00F3n.');
+		window.location.href='http://www.euweloboost.com';
+	</script>
+	<?php
+}
+
+//header ("Location: http://www.euweloboost.com");
+?>
